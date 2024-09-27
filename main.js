f87b4fa9db23608ca37ce61e3798b2a1ea572ccb
henriquemacielr <79269898+henriquemacielr@users.noreply.github.com>
2024-09-27 17:27:46 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function calcular() {
+    
+    let horaIni = parseInt(document.getElementById('horaIni').value);
+    let minIni = parseInt(document.getElementById('minIni').value);
+    let horaFim = parseInt(document.getElementById('horaFim').value);
+    let minFim = parseInt(document.getElementById('minFim').value);
+
+    let inicioEmMinutos = (horaIni * 60) + minIni;
+    let fimEmMinutos = (horaFim * 60) + minFim;
+
+    
+    let duracaoEmMinutos = fimEmMinutos - inicioEmMinutos;
+
+    let horas = Math.floor(duracaoEmMinutos / 60);
+    let minutos = duracaoEmMinutos % 60;
+    
+    if (horas < 10) {
+        document.getElementById('horas').innerText = "0" + horas;
+    } else {
+        document.getElementById('horas').innerText = horas;
+    }
+    
+    
+    if (minutos < 10) {
+        document.getElementById('minutos').innerText = "0" + minutos;
+    } else {
+        document.getElementById('minutos').innerText = minutos;
+    }
+}
